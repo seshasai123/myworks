06387b9d5e4d6e3a98fdf607f892a165a498c5e4
seshasai123 <saiseshu84@gmail.com>
2016-06-29 23:33:51 -0400

    CHanging the text

---
+++ src/main/java/com/learn/core/App.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.learn.core;
+
+/**
+ * @author SeshaSaiVenkata
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("seshi.....");
+	}
+
+}
